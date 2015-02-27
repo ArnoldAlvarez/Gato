2fe7578eac35bfa668fe9f1f18018a7cad963125
Arnold Alvarez Perez <arnold_alfonso@hotmail.com>
2015-02-27 10:22:32 -0700

    Gato

---
+++ Gato/src/Main.java
@@ -0,0 +1,23 @@
+
+import javax.swing.*;
+
+/*
+ * Clase que contiene
+ * el metodo main de 
+ * la aplicacion el el
+ * look and feel del juego
+ * 
+ */
+
+/**
+ * @author Carlos Mario Montaño Lopera
+ * @version 1.0.0.1
+ * @
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Tablero mesa = new Tablero();
+
+    }
+}
